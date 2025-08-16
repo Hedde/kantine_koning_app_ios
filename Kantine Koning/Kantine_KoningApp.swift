6ae4011c821821298539fe1f2b82a9a0ece94c9d
Hedde van der Heide <hedde.vanderheide@ictu.nl>
2025-08-16 16:59:37 +0200

    Initial Commit

---
+++ Kantine Koning/Kantine_KoningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kantine_KoningApp.swift
+//  Kantine Koning
+//
+//  Created by Hedde van der Heide on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Kantine_KoningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
